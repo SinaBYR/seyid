80b48f55bb81434f40931e183c248d8b99c5bc23
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-09-28 20:24:23 +0330

    add database init file

---
+++ lib/db.go
@@ -0,0 +1,22 @@
+package lib
+
+import "database/sql"
+
+func InitDatabase () *sql.DB {
+	connStr := "user=postgres dbname=seyid password=Sina13801111 host=localhost port=5432 sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
