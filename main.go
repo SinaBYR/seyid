4a1983150214f491d2587ddf19c621fca0f14dab
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-09-28 22:23:19 +0330

    serve static content under /static url path

--- main.go
+++ main.go
@@ -1,10 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"net/http"
+	_ "github.com/lib/pq"
+
+	"sinabyr/seyid/lib"
 )
 
+type Film struct {
+	// Id int64
+	Title string
+	Director string
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	db := lib.InitDatabase()
+
+	rows, err := db.Query("SELECT * FROM films;")
+	if err != nil {
+		panic(err)
+	}
+
+	var Films []Film
+	for rows.Next() {
+		var id int64
+		var title string
+		var director string
+
+		err := rows.Scan(&id, &title, &director)
+		if err != nil {
+			panic(err)
+		}
+
+		Films = append(Films, Film{Title: title, Director: director})
+	}
+
+	data := map[string][]Film{ "films": Films } 
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, data)
+}
+
+func createReceipt(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	fmt.Printf("%s\n", "Hello world")
+	// serve "./public" directory contents under "/static" url path
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
+	http.HandleFunc("/", homeHandler)
+
+	http.ListenAndServe(":8000", nil)
 }
 
